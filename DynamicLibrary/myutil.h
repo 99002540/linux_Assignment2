4431230031a5375e088f85c55b4e46fc6e430b48
99002540 <71496134+99002540@users.noreply.github.com>
2020-10-15 09:52:11 +0530

    Add files via upload

---
+++ DynamicLibrary/myutil.h
@@ -0,0 +1,10 @@
+#ifndef UTIL_H_INCLUDED
+#define UTIL_H_INCLUDED
+
+int prime(int number);
+void fact(int number);
+void checkpalindrome(int number);
+int addition(int num, ...);
+
+
+#endif // UTIL_H_INCLUDED
